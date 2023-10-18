18aac376711642068065891c1e15cce5bf7e3515
Francis <710892027dongyi@gmail.com>
2023-10-18 15:50:47 +0800

    walletconnect/modal-react-native done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import { registerRootComponent } from "expo";
+
+import {
+  WalletConnectModal,
+  useWalletConnectModal,
+} from "@walletconnect/modal-react-native";
+// Add in the useWalletConnectModal hook
+
+// TODO: Insert the ProjectID
+const projectId = "0c975f1f6d26007c1e2fa3378373cdb2";
+
+const providerMetadata = {
+  name: "YOUR_PROJECT_NAME",
+  description: "YOUR_PROJECT_DESCRIPTION",
+  url: "https://your-project-website.com/",
+  icons: ["https://your-project-logo.com/"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
 
 export default function App() {
+  // Add in the useWalletConnectModal hook + props
+  const { open, isConnected, address, provider } = useWalletConnectModal();
+
+  // Function to handle the
+  const handleButtonPress = async () => {
+    if (isConnected) {
+      return provider?.disconnect();
+    }
+    return open();
+  };
+
+  // Main UI Render
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>WalletConnect Modal RN Tutorial</Text>
+      <Text>{isConnected ? address : "Status: Not Connected"}</Text>
+      <Pressable onPress={handleButtonPress} style={styles.pressableMargin}>
+        <Text>{isConnected ? "Disconnect" : "Connect"}</Text>
+      </Pressable>
+
+      <WalletConnectModal
+        projectId={projectId}
+        providerMetadata={providerMetadata}
+      />
     </View>
   );
 }
@@ -13,8 +53,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  pressableMargin: {
+    marginTop: 16,
   },
 });
+
+registerRootComponent(App);
